e12e803b39501029e2f10096ccf89432374fb01a
DarkLite1 <BrechtGijbels@gmail.com>
2024-07-17 11:56:35 +0200

    add script to remove files

---
+++ Remove file.Tests.ps1
@@ -0,0 +1,54 @@
+﻿#Requires -Modules Pester
+#Requires -Version 7
+
+BeforeAll {
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testParams = @{
+        Path              = (New-Item 'TestDrive:/a' -ItemType File).FullName
+        OlderThanUnit     = 'Year'
+        OlderThanQuantity = 0
+    }
+}
+Describe 'the mandatory parameters are' {
+    It '<_>' -ForEach @(
+        'Path', 'OlderThanUnit', 'OlderThanQuantity'
+    ) {
+        (Get-Command $testScript).Parameters[$_].Attributes.Mandatory |
+        Should -BeTrue
+    }
+}
+Describe 'remove' {
+    BeforeAll {
+        $test = @{
+            Files   = @(
+                "TestDrive:/b",
+                "TestDrive:/c"
+            ).ForEach(
+                { New-Item $_ -ItemType File }
+            )
+            Folders = @(
+                "TestDrive:/f1",
+                "TestDrive:/f2"
+            ).ForEach(
+                { New-Item $_ -ItemType Directory }
+            )
+        }
+
+        . $testScript @testParams
+    }
+    It 'the requested file' {
+        $testParams.Path | Should -Not -Exist
+    }
+    Context 'do not remove' {
+        It 'other files' {
+            $test.Files.foreach(
+                { $_.FullName | Should -Exist }
+            )
+        }
+        It 'other folders' {
+            $test.Folders.foreach(
+                { $_.FullName | Should -Exist }
+            )
+        }
+    }
+}
---
+++ Remove file.ps1
@@ -0,0 +1,107 @@
+﻿Param (
+    [Parameter(Mandatory)]
+    [String]$Path,
+    [Parameter(Mandatory)]
+    [ValidateSet('Day', 'Month', 'Year')]
+    [String]$OlderThanUnit,
+    [Parameter(Mandatory)]
+    [Int]$OlderThanQuantity
+)
+
+#region Test file exists
+if (-not (Test-Path -LiteralPath $Path -PathType 'Leaf')) {
+    return [PSCustomObject]@{
+        ComputerName = $env:COMPUTERNAME
+        Type         = 'File'
+        FullName     = $null
+        CreationTime = $null
+        Action       = $null
+        Error        = 'Path not found'
+    }
+}
+#endregion
+
+#region Create filter
+Write-Verbose "Create filter for files with a creation date older than '$OlderThanQuantity $OlderThanUnit'"
+
+if ($OlderThanQuantity -eq 0) {
+    Filter Select-FileHC {
+        Write-Output $_
+    }
+}
+else {
+    $today = Get-Date
+
+    Switch ($OlderThanUnit) {
+        'Day' {
+            Filter Select-FileHC {
+                if (
+                    $_.CreationTime.Date.ToString('yyyyMMdd') -le $(($today.AddDays( - $OlderThanQuantity)).Date.ToString('yyyyMMdd'))
+                ) {
+                    Write-Output $_
+                }
+            }
+
+            break
+        }
+        'Month' {
+            Filter Select-FileHC {
+                if (
+                    $_.CreationTime.Date.ToString('yyyyMM') -le $(($today.AddMonths( - $OlderThanQuantity)).Date.ToString('yyyyMM'))
+                ) {
+                    Write-Output $_
+                }
+            }
+
+            break
+        }
+        'Year' {
+            Filter Select-FileHC {
+                if (
+                    $_.CreationTime.Date.ToString('yyyy') -le $(($today.AddYears( - $OlderThanQuantity)).Date.ToString('yyyy'))
+                ) {
+                    Write-Output $_
+                }
+            }
+
+            break
+        }
+        Default {
+            throw "OlderThan.Unit '$_' not supported"
+        }
+    }
+}
+#endregion
+
+Get-Item -LiteralPath $Path -ErrorAction Stop | Select-FileHC | ForEach-Object {
+    try {
+        Write-Verbose "Remove file '$Path'"
+
+        $result = [PSCustomObject]@{
+            ComputerName = $env:COMPUTERNAME
+            Type         = 'File'
+            FullName     = $_.FullName
+            CreationTime = $_.CreationTime
+            Action       = $null
+            Error        = $null
+        }
+
+        $params = @{
+            LiteralPath = $_.FullName
+            Force       = $true
+            ErrorAction = 'Stop'
+        }
+        Remove-Item @params
+
+        $result.Action = 'Removed'
+    }
+    catch {
+        Write-Warning "Failed to remove file '$Path': $_"
+
+        $result.Error = $_
+        $Error.RemoveAt(0)
+    }
+    finally {
+        $result
+    }
+}
